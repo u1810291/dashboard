e0f943cebb14eaf0b1cfc7cef27b2577e01ba5ef
Vadim Rastyagaev <vadim.rastyagaev@toptal.com>
2018-09-17 16:50:46 +0300

    fix missed conflicts

--- src/lib/client/http.js
+++ src/lib/client/http.js
@@ -1,11 +1,7 @@
 import axios from 'axios'
 
 const http = axios.create({
-<<<<<<< HEAD
   baseURL: 'https://staging.mati.io/api/v1'
-=======
-  baseURL: 'https://dev.mati.io/api/v1'
->>>>>>> 177bac9d4003547904d96548d23d691f35e2b71e
 })
 
 export default http
