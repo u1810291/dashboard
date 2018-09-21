56cdc97a73c07d5bc9fee131ad444acb5e4501a3
Ernesto Palafox <palafox.ernesto@gmail.com>
2018-09-21 13:55:02 -0500

    Change http for https

--- src/lib/client/http.js
+++ src/lib/client/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const http = axios.create({
-  baseURL: 'http://api.globalid.mati.io/api/v1'
+  baseURL: 'https://api.globalid.mati.io/api/v1'
 })
 
 export default http
