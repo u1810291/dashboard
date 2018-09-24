deaa820a92a0c1c21a88a120d6c7a0c2b5b9ca7e
Vadim Rastyagaev <vadim.rastyagaev@toptal.com>
2018-09-24 16:21:53 +0200

    update API server root with https prefix

--- src/lib/client/http.js
+++ src/lib/client/http.js
@@ -1,7 +1,11 @@
 import axios from 'axios'
 
 const http = axios.create({
-  baseURL: 'http://api.globalid.mati.io/api/v1'
+  baseURL: 'https://api.globalid.mati.io/api/v1'
 })
 
 export default http
+
+export function getAuthHeader(token) {
+  return { Authorization: `Bearer ${token}` }
+}
