ec828b020bbc2dc204e78459499c1b6f14561634
Vadim Rastyagaev <vadim.rastyagaev@toptal.com>
2018-09-25 01:36:57 +0200

    Matibutton

--- src/components/mati-button/index.js
+++ src/components/mati-button/index.js
@@ -2,16 +2,26 @@ import React from 'react'
 
 export class MatiButton extends React.Component {
   componentDidMount() {
-    window.Mati.init({
-      clientId: '5b50cd531a76d77ba81818e2',
-    })
+    window.Mati.render({
+      clientId: '5ae0c3fbb01a23351872ab18',
+      element: this.element,
+      hideChat: true,
+      metadata:  {
+        userId: 101150998,
+        medio: 'PORTAL',
+        token: 6150000227,
+        identityId: '5b89d66b1222051ccf956362'
+      },
+    });
   }
   render() {
     return (
       <div
         className="mati-button"
         data-product="kyc"
-      ></div>
+        ref={element => this.element = element}
+      >
+      </div>
     )
   }
 }
