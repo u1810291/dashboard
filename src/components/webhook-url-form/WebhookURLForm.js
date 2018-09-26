5023ba29cc88f7bd516f84b17001d2b39b795bea
Vadim Rastyagaev <vadim.rastyagaev@toptal.com>
2018-09-26 11:58:09 +0200

    Webhook URL saving confirmation

--- src/components/webhook-url-form/WebhookURLForm.js
+++ src/components/webhook-url-form/WebhookURLForm.js
@@ -11,32 +11,61 @@ const formikSettings = {
     const { url } = values
     const { token } = props
     setStatus({})
-    props.subscribeToWebhook(token, { url, secret: token })
-    .then(data => {
-      setSubmitting(false)
-    })
-    .catch(error => {
-      const status = {}
-      error.response.data.details.forEach(e => {
-        status[e.path.toString()] = e.message
+    props
+      .subscribeToWebhook(token, { url, secret: token })
+      .then(data => {
+        setSubmitting(false)
+        setStatus({ success: true })
       })
-      setSubmitting(false)
-      setStatus(status)
-    })
+      .catch(error => {
+        const status = {}
+        error.response.data.details.forEach(e => {
+          status[e.path.toString()] = e.message
+        })
+        setSubmitting(false)
+        setStatus(status)
+      })
   }
 }
 
 @setI18nContext('onboarding.webhooks')
 @withFormik(formikSettings)
 export default class WebhookURLForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showConfirmation: false
+    }
+  }
+
+  componentDidUpdate(props) {
+    if (
+      props.status &&
+      this.props.status.success !== props.status.success &&
+      this.props.status.success
+    ) {
+      this.setState({ showConfirmation: true })
+      setInterval(() => {
+        this.setState({ showConfirmation: false })
+      }, 3000)
+    }
+  }
+
   render() {
     return (
-      <Form className={CSS.form}>
-        <Field name="url" hideLabel className={CSS.input} component={Input} />
-        <Button type="submit" buttonStyle="primary">
-          <FormattedMessage id="webhookUrl.save" />
-        </Button>
-      </Form>
+      <React.Fragment>
+        <Form className={CSS.form}>
+          <Field name="url" hideLabel className={CSS.input} component={Input} />
+          <Button type="submit" buttonStyle="primary">
+            <FormattedMessage id="webhookUrl.save" />
+          </Button>
+        </Form>
+        {this.state.showConfirmation && (
+          <span className="text-secondary">
+            <FormattedMessage id="webhookUrl.confirmation" />
+          </span>
+        )}
+      </React.Fragment>
     )
   }
 }
