e6c27a23f65de10388ac5429996e338d85ae8ebc
Eugene Gordeev <eugeny.gordeev@gmail.com>
2018-11-23 18:06:00 +0300

    Added webhook examples

--- src/apps/developers/index.js
+++ src/apps/developers/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { injectIntl } from 'react-intl'
+import { injectIntl, FormattedMessage } from 'react-intl'
 import {
   subscribeToWebhook,
   deleteWebhook,
@@ -11,12 +11,9 @@ import { sendNotification } from 'src/components/notification'
 import { Content } from 'src/components/application-box'
 import { WebhookURLForm } from 'src/components/webhook-url-form'
 import Icons from 'src/components/icons'
-import { Tabs, TabList, Tab, TabPanel } from 'src/components/tabs'
-import classNames from 'classnames'
+import WebhookExamples from 'src/components/webhook-examples'
 import Panel from 'src/components/panel'
-import { Input } from 'src/components/inputs'
-import Button from 'src/components/button'
-import CSS from './styles.scss'
+import CSS from './style.scss'
 
 export default
 @connect(
@@ -56,16 +53,11 @@ class Developers extends React.Component {
   render() {
     return (
       <Content>
-        <Panel caption="Webhook">
+        <Panel caption={this.props.intl.formatMessage({ id: 'webhook' })}>
           <Panel.Header>
             <Icons.Info />
             <span className={CSS.webhookUrlHeader}>
-              The weekhook is the way to receive the verification information on
-              your backend. You will receive all user information: images of
-              selfie & documents, document information (full name, document
-              number, face match, and more), liveness score, global watchlist,
-              etc... With this information on your backend, you will be able to
-              fully automate your verifiation flow.
+              <FormattedMessage id="developers.webhook.form.description" />
             </span>
           </Panel.Header>
           <Panel.Body>
@@ -75,41 +67,14 @@ class Developers extends React.Component {
             />
           </Panel.Body>
         </Panel>
-        <Panel caption="Developers integration sheet">
+        <Panel
+          caption={this.props.intl.formatMessage({
+            id: 'developers.webhook.example.header'
+          })}
+        >
           <Panel.Body padded={false}>
-            <Tabs className={classNames('tabs', 'stretched')}>
-              <TabList>
-                <Tab>
-                  <Icons.Spy /> <span>Global Watchlist</span>
-                </Tab>
-                <Tab>
-                  <Icons.FaceMatch /> <span>Face Match</span>
-                </Tab>
-                <Tab>
-                  <Icons.TickFilled /> <span>Liveness</span>
-                </Tab>
-                <Tab>
-                  <Icons.WarningTriangle /> <span>OCR data</span>
-                </Tab>
-              </TabList>
-              <TabPanel />
-              <TabPanel />
-              <TabPanel />
-              <TabPanel />
-            </Tabs>
+            <WebhookExamples />
           </Panel.Body>
-        </Panel>
-        <Panel caption="How to fetch images">
-          <Panel.Header>
-            <div>
-              <Icons.Info />
-            </div>
-            <span>
-              Instruction here how to GET sample to fetch images in the webhook
-              response
-            </span>
-          </Panel.Header>
-          <Panel.Body />
         </Panel>
       </Content>
     )
