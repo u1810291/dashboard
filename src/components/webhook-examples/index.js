c2745aa931d97af3ed8c0a2577a4af4dd5eec6f3
Eugene Gordeev <eugeny.gordeev@gmail.com>
2018-11-26 14:48:45 +0300

    Added proper webhook examples

--- src/components/webhook-examples/index.js
+++ src/components/webhook-examples/index.js
@@ -1,45 +1,10 @@
 import React, { Component } from 'react'
 import { injectIntl, FormattedMessage } from 'react-intl'
 import { SyntaxHighlighter } from 'src/components/syntax-highlighter'
-import Icons from 'src/components/icons'
 import { Tabs, TabList, Tab, TabPanel } from 'src/components/tabs'
+import WEBHOOK_EXAMPLES_TABS from './webhook-examples-tabs'
 import classNames from 'classnames'
 import CSS from './style.scss'
-
-const TABS_CONFIG = [
-  {
-    label: 'global_watchlist',
-    icon: <Icons.Spy />,
-    webhooks: [
-      { label: 'developers.webhook.example.user.verified', response: '{}' },
-      { label: 'developers.webhook.example.user.found.watchlist', response: '{}' }
-    ]
-  },
-  {
-    label: 'face_match',
-    icon: <Icons.FaceMatch />,
-    webhooks: [
-      { label: 'developers.webhook.example.user.verified', response: '{}' },
-      { label: 'developers.webhook.example.user.found.watchlist', response: '{}' }
-    ]
-  },
-  {
-    label: 'liveness',
-    icon: <Icons.TickFilled />,
-    webhooks: [
-      { label: 'developers.webhook.example.user.verified', response: '{}' },
-      { label: 'developers.webhook.example.user.found.watchlist', response: '{}' }
-    ]
-  },
-  {
-    label: 'ocr_data',
-    icon: <Icons.WarningTriangle />,
-    webhooks: [
-      { label: 'developers.webhook.example.user.verified', response: '{}' },
-      { label: 'developers.webhook.example.user.found.watchlist', response: '{}' }
-    ]
-  }
-]
 
 export default
 @injectIntl
@@ -48,34 +13,44 @@ class extends Component {
     return (
       <Tabs className={classNames('tabs', 'stretched')}>
         <TabList>
-          {TABS_CONFIG.map(tab => {
+          {WEBHOOK_EXAMPLES_TABS.map((tab, index) => {
             return (
-              <Tab>
+              <Tab key={index}>
                 {tab.icon} <FormattedMessage id={tab.label} />
               </Tab>
             )
           })}
         </TabList>
-        {TABS_CONFIG.map(tab => {
+        {WEBHOOK_EXAMPLES_TABS.map((tab, index) => {
           return (
-            <TabPanel>
+            <TabPanel key={index}>
               <div className={CSS.webhookExamples}>
                 <div className={CSS.webhookExample}>
-                  <h5>
+                  <h5 className="text-success">
                     <FormattedMessage id={tab.webhooks[0].label} />
                   </h5>
                   <SyntaxHighlighter
                     language="json"
+                    wrapperClassName={CSS['webhook-example__code-wrapper']}
                     code={tab.webhooks[0].response}
                   />
                 </div>
                 <div className={CSS.webhookExample}>
-                  <h5>
+                  <h5 className="text-warning">
                     <FormattedMessage id={tab.webhooks[1].label} />
                   </h5>
                   <SyntaxHighlighter
                     language="json"
                     code={tab.webhooks[1].response}
+                    wrapperClassName={CSS['webhook-example__code-wrapper']}
+                    wrapLines={true}
+                    lineProps={line => {
+                      return tab.webhooks[1].highlightedLines.includes(line)
+                        ? {
+                            className: 'webhook-example__highlighted-line'
+                          }
+                        : null
+                    }}
                   />
                 </div>
               </div>
