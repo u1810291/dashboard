a0ace9420d643c208845ced0e1703c6ca8edf475
Lenar Fattakhov <lenar.fattakhov@mati.io>
2019-02-19 12:22:56 +0000

    Merged in feature/add-multiselect-component (pull request #83)
    Add Multiselect component, based on react-select
    
    Approved-by: Vadim Rastyagaev <rastyagaev@gmail.com>

---
+++ src/components/multi-select/MultiSelect.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import classNames from 'classnames'
+import CSS from './MultiSelect.scss'
+import Select from 'react-select'
+
+export default class MultiSelect extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      value: this.props.value || []
+    }
+  }
+
+  onChange = (value) => {
+    this.setState({
+      value
+    })
+    this.props.onChange && this.props.onChange(value)
+  }
+
+  deleteOption = (optionIndex) => {
+    this.setState({
+      value: this.state.value.filter((option, index) => index !== optionIndex)
+    })
+  }
+
+  render() {
+    const {
+      className,
+      selectClassName,
+      valuesClassName,
+      ...inputOptions
+    } = this.props
+    const { value } = this.state
+
+    return (
+      <div className={classNames(CSS.multiSelect, className)}>
+        <Select
+          isMulti
+          {...inputOptions}
+          className={classNames(CSS.select, selectClassName)}
+          value={value}
+          isSearchable
+          controlShouldRenderValue={false}
+          onChange={this.onChange}
+        />
+        <div className={valuesClassName}>
+          {value.map((option, index) =>
+            <button
+              key={index}
+              className={CSS.valueItem}
+              onClick={() => this.deleteOption(index)}
+            >{option.value}</button>
+          )}
+        </div>
+      </div>
+    )
+  }
+}
