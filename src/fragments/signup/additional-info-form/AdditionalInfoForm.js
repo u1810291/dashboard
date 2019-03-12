37e046e6368c6f9e4a182e7515c140a0ba1ddd43
Lenar Fattakhov <lenar.fattakhov@mati.io>
2019-03-12 16:09:22 +0000

    Merged in bugfix/fix-typ (pull request #110)
    Bugfix/fix typ
    * Fix thank you page redirect

--- src/fragments/signup/additional-info-form/AdditionalInfoForm.js
+++ src/fragments/signup/additional-info-form/AdditionalInfoForm.js
@@ -2,12 +2,10 @@ import React from 'react'
 import CSS from './AdditionalInfoForm.css'
 import { Input } from 'src/components/inputs'
 import Button from 'src/components/button'
-import { Link } from 'react-router-dom'
 import { Field, Formik } from 'formik'
 import { setI18nContext } from 'src/components/i18n-context'
 import { FormattedMessage } from 'react-intl'
 import { pick } from 'lodash'
-import { showVideo } from 'src/fragments/configuration/onboarding-video'
 
 export default
 @setI18nContext('additionalInfo.form')
@@ -19,18 +17,12 @@ class AdditionalInfoForm extends React.Component {
       .handleSubmit(data)
       .then(() => {
         setSubmitting(false)
-        this.showOnboardingVideo()
       })
       .catch(error => {
         setSubmitting(false)
         setStatus({ websiteUrl: error.response.data.message })
       })
   }
-
-  showOnboardingVideo = () => {
-    setTimeout(() => showVideo(), 1000)
-  }
-
   render() {
     return (
       <Formik
@@ -40,7 +32,7 @@ class AdditionalInfoForm extends React.Component {
             <Field type="text" name="businessName" component={Input} />
             <Field type="text" name="websiteUrl" component={Input} />
             <p>
-              <Link to="/" onClick={this.showOnboardingVideo} className={CSS.skipLink}>
+              <a href="/" className={CSS.skipLink}>
                 <Button
                   className={CSS.submit}
                   disabled={props.isSubmitting}
@@ -48,7 +40,7 @@ class AdditionalInfoForm extends React.Component {
                 >
                   <FormattedMessage id="additionalInfo.skip" />
                 </Button>
-              </Link>
+              </a>
               <Button
                 type="submit"
                 className={CSS.submit}
