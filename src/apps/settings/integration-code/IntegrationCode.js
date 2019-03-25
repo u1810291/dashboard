52472ab59a8c22d0b6bbac5294b370f170585cd8
Alex Kovalchuk <alexkovalchuk@mati.io>
2019-03-25 15:51:10 +0000

    Merged in BAC-2779-change-block-with-documentation-on-integration-code (pull request #139)
    BAC-2779 Change block with Documentation on Integration Code page
    
    Approved-by: Vadim Rastyagaev <rastyagaev@gmail.com>

--- src/apps/settings/integration-code/IntegrationCode.js
+++ src/apps/settings/integration-code/IntegrationCode.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import Panel from 'src/components/panel'
 import { SyntaxHighlighter } from 'src/components/syntax-highlighter'
 import { getIntegrationCode } from 'src/state/merchant'
 import Support from 'src/fragments/account/support'
+import MatiDocs from 'src/fragments/account/mati-docs'
 import { FormattedMessage } from 'react-intl'
 import SettingsLayout from '../SettingsLayout'
 
@@ -28,7 +28,9 @@ class IntegrationCode extends React.Component {
         <h1>
           <FormattedMessage id="settings.integrationCode.title" />
         </h1>
+
         <SettingsLayout>
+
           <main>
             <SyntaxHighlighter
               dark={false}
@@ -36,40 +38,13 @@ class IntegrationCode extends React.Component {
               copyToClipboard
               code={this.props.integrationCode}
             />
+            <MatiDocs />
           </main>
 
           <aside>
-            <Panel>
-              <Panel.Header>
-                <FormattedMessage id="settings.integrationCode.matiDocumentation" />
-              </Panel.Header>
-              <Panel.Body>
-                <h3>
-                  <FormattedMessage id="settings.integrationCode.webhookDocumentation" />
-                  <p>
-                    <a href="https://docs.getmati.com">https://docs.getmati.com</a>
-                  </p>
-                </h3>
-                <h3>
-                  <FormattedMessage id="settings.integrationCode.iosDocumentation" />
-                  <p>
-                    <a href="https://github.com/MatiFace/mati-global-id-sdk/blob/master/Integration_iOS.md">
-                      https://github.com/MatiFace/mati-global-id-sdk/blob/master/Integration_iOS.md
-                    </a>
-                  </p>
-                </h3>
-                <h3>
-                  <FormattedMessage id="settings.integrationCode.androidDocumentation" />
-                  <p>
-                    <a href="https://github.com/MatiFace/mati-global-id-sdk/blob/master/Integration_android.md">
-                      https://github.com/MatiFace/mati-global-id-sdk/blob/master/Integration_android.md
-                    </a>
-                  </p>
-                </h3>
-              </Panel.Body>
-            </Panel>
             <Support />
           </aside>
+
         </SettingsLayout>
       </React.Fragment>
     )
---
+++ src/fragments/account/mati-docs/MatiDocs.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { FormattedMessage } from 'react-intl'
+import Panel from 'src/components/panel'
+import CSS from './mati-docs.scss'
+import IconApple from '/src/assets/icon-apple-documents.svg'
+import IconAndroid from '/src/assets/icon-android-documents.svg'
+import IconWebhook from '/src/assets/icon-webhook.svg'
+
+export default function MatiDocs() {
+
+  return (
+    <div className={CSS.content}>
+      <Panel>
+        <Panel.Body>
+          <h3>
+            <FormattedMessage id="settings.integrationCode.matiDocumentation" />
+          </h3>
+          <div className="mgi-items text-center">
+            <div className="doc-item">
+              <a href="https://github.com/MatiFace/mati-global-id-sdk/blob/master/Integration_iOS.md">
+                <div className="doc-item--icon">
+                  <IconApple />
+                </div>
+                <p>
+                  <FormattedMessage id="settings.integrationCode.iosDocumentation" />
+                </p>
+              </a>
+            </div>
+            <div className="doc-item">
+              <a href="https://github.com/MatiFace/mati-global-id-sdk/blob/master/Integration_android.md">
+                <div className="doc-item--icon">
+                  <IconAndroid />
+                </div>
+                <p>
+                  <FormattedMessage id="settings.integrationCode.androidDocumentation" />
+                </p>
+              </a>
+            </div>
+            <div className="doc-item">
+              <a href="https://docs.getmati.com">
+                <div className="doc-item--icon">
+                  <IconWebhook />
+                </div>
+                <p>
+                  <FormattedMessage id="settings.integrationCode.webhookDocumentation" />
+                </p>
+              </a>
+            </div>
+          </div>
+        </Panel.Body>
+      </Panel>
+    </div>
+  )
+}
