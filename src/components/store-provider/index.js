d565cb932635260b7e51ce4daa191823c65cbbbe
Vadim Rastyagaev <rastyagaev@gmail.com>
2019-05-06 20:06:02 +0300

    HOTFIX redux cache

--- src/components/store-provider/index.js
+++ src/components/store-provider/index.js
@@ -11,7 +11,7 @@ import * as reducers from 'src/state/reducers'
 
 const persistConfig = {
   key: 'mgi-dashboard-3',
-  whitelist: ['auth', 'webhooks', 'merchant', 'countries'],
+  whitelist: ['auth', 'webhooks', 'countries'],
   storage
 }
 
