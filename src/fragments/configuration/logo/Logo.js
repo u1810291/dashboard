5252bf9fe5c69eea430a983b26c6d5ec66273126
Alexandra Faleychik <alexandra.faleychik@mati.io>
2019-09-02 08:19:33 +0000

    Merged in BAC-4163/fix-black-background (pull request #316)
    fix black background
    
    Approved-by: Vyacheslav Frolov <vyacheslav.frolov@mati.io>

--- src/fragments/configuration/logo/Logo.js
+++ src/fragments/configuration/logo/Logo.js
@@ -36,14 +36,16 @@ export default function Logo() {
   const onDropAccepted = useCallback(async files => {
     try {
       const file = files[0];
+      const compressionOptions = {
+        maxSideSize: 159,
+        type: file.type === 'image/png' ? file.type : 'image/jpeg'
+      };
       const form = new FormData();
-      const compressedFile = await compressImage(file, { maxSideSize: 159 });
+      const compressedFile = await compressImage(file, compressionOptions);
       form.append('media', compressedFile);
-
       await dispatch(
         uploadMerchantMedia(token, form),
       );
-
       setShouldLogoUpdate(true);
     } catch (error) {
       showError();
