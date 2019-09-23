21f725dc789abcff9ed34aaaa314004a24ff1e07
Vyacheslav <algebris@gmail.com>
2019-09-24 04:22:08 +0700

    BAC-4278 change color

--- src/components/text/Text.js
+++ src/components/text/Text.js
@@ -14,13 +14,18 @@ const Text = styled.span(
     size = 2,
     textDecoration = 'none',
     uppercase,
+    capitalize,
     weight = 2,
   }) => ({
     fontSize: size === 2 ? null : `${1 + 0.28 * (size - 2)}rem`,
     color: color ? `var(--mgi-theme-palette-${color})` : null,
     fontWeight: weight === 2 ? null : 400 + 100 * (weight - 2),
     lineHeight,
-    textTransform: uppercase ? 'uppercase' : null,
+    textTransform: (() => {
+      if (uppercase) return 'uppercase';
+      if (capitalize) return 'capitalize';
+      return 'null';
+    })(),
     textAlign: align,
     padding,
     opacity,
