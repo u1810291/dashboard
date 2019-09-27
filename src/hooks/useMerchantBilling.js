b23fc38836dd1b36a1c005c2f178a3126fa1ea2c
Bulakh Serhii <serhii.bulakh@vedidev.com>
2019-09-27 19:01:55 +0300

    BAC-4415 Unlock integration page keys - Destruct get

--- src/hooks/useMerchantBilling.js
+++ src/hooks/useMerchantBilling.js
@@ -1,13 +1,13 @@
-import _ from 'lodash';
+import { get } from 'lodash';
 import { useSelector } from 'react-redux';
 
 export default () => {
   const merchant = useSelector((state) => state.merchant);
 
-  const providers = _.get(merchant, 'billing.providers', []);
+  const providers = get(merchant, 'billing.providers', []);
   const hasProvider = providers.length;
-  const isPlanActivated = _.get(merchant, 'billing.planDetails.activatedAt')
-    && _.get(merchant, 'billing.planDetails.plan');
+  const isPlanActivated = get(merchant, 'billing.planDetails.activatedAt')
+    && get(merchant, 'billing.planDetails.plan');
 
   return {
     hasProvider,
