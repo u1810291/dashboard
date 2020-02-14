84a76ddb1036a7a787160e8e170e56cacca82abf
vyacheslav.frolov <vyacheslav.frolov@mati.io>
2020-02-14 01:40:43 +0000

    BAC-5561 gov checks

---
+++ src/fragments/configuration/gov-checks/icons/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ReactComponent as MexicoIcon } from './mexico-flag.svg';
+import { ReactComponent as RussiaIcon } from './russia-flag.svg';
+import { ReactComponent as USAIcon } from './usa-flag.svg';
+
+export const flags = {
+  mexico: <MexicoIcon width="25" height="25" />,
+  russia: <RussiaIcon width="25" height="25" />,
+  usa: <USAIcon width="25" height="25" />,
+};
