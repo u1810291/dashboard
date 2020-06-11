739912200a26fdd3bdc16fd53c9e50f4d27f516f
dkchv <anton@dkchv.ru>
2020-06-11 14:54:29 +0300

    FNX-221 update mx check labels

--- src/apps/identity/components/DocumentStep/CheckBar/components/CheckStepDetails/CheckStepDetails.js
+++ src/apps/identity/components/DocumentStep/CheckBar/components/CheckStepDetails/CheckStepDetails.js
@@ -1,8 +1,7 @@
+import { Card, CardContent, Grid, Typography } from '@material-ui/core';
+import { formatValue } from 'lib/string';
 import React from 'react';
 import { useIntl } from 'react-intl';
-import { humanize, underscore } from 'inflection';
-import { formatValue } from 'lib/string';
-import { Card, Grid, CardContent, Typography } from '@material-ui/core';
 import { useStyles } from './CheckStepDetails.styles';
 
 export function CheckStepDetails({ data = {}, error }) {
@@ -13,27 +12,26 @@ export function CheckStepDetails({ data = {}, error }) {
     <Card raised={false} className={classes.card}>
       <CardContent>
         <Grid container spacing={1}>
-          { error && (
+          {error && (
             <Grid container item>
               <Typography color="error">
                 {intl.formatMessage({ id: `SecurityCheckStep.${error.code}.message` })}
               </Typography>
             </Grid>
           )}
-          { Object.entries(data)
-            .map(([label, { value }]) => (
-              <Grid container item spacing={1} key={label}>
-                <Grid item xs={3} className={classes.label}>
-                  {intl.formatMessage({
-                    id: `identity.field.${label}`,
-                    defaultMessage: humanize(underscore(label)),
-                  })}
-                </Grid>
-                <Grid item xs={3} className={classes.value}>
-                  {formatValue(label, value)}
-                </Grid>
+          {Object.entries(data).map(([key, value]) => (
+            <Grid container item spacing={1} key={key}>
+              <Grid item xs={3} className={classes.label}>
+                {intl.formatMessage({
+                  id: `identity.field.${key}`,
+                  defaultMessage: key,
+                })}
               </Grid>
-            ))}
+              <Grid item xs={3} className={classes.value}>
+                {formatValue(key, value)}
+              </Grid>
+            </Grid>
+          ))}
         </Grid>
       </CardContent>
     </Card>
