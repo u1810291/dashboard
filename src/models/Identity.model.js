3dd5106056026f2af6e61644d4bad8be374cf686
dkchv <anton@dkchv.ru>
2020-09-09 12:25:41 +0300

    FNX-601 fix color

--- src/models/Identity.model.js
+++ src/models/Identity.model.js
@@ -19,7 +19,7 @@ export const IdentityStatuses = {
   unknown: 'unknown',
 };
 
-export const DEFAULT_STATUS_COLOR = '#C4C4C4';
+export const DEFAULT_STATUS_COLOR = '#ADADAD';
 
 export const IdentityStatusesMap = [
   {
