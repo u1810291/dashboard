f871a21305fbed06b08fffc87d1b1ef2839d887e
Vladislav Snimshchikov <vsnimshchikov@emphasoft.com>
2020-10-26 12:29:00 +0300

    Manual review count fix

--- src/apps/identity/components/ManualReviewBanner/ManualReviewBanner.js
+++ src/apps/identity/components/ManualReviewBanner/ManualReviewBanner.js
@@ -2,9 +2,10 @@ import { Button, Grid, Paper, Typography } from '@material-ui/core';
 import React, { useCallback, useEffect, useState } from 'react';
 import { FiX } from 'react-icons/fi';
 import { useIntl } from 'react-intl';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { IdentityStatuses } from '../../../../models/Identity.model';
-import { selectManualReviewCountModel } from '../../../../state/identities/identities.selectors';
+import { identitiesManualReviewCountLoad } from '../../../../state/identities/identities.actions';
+import { selectIdentityCountModel, selectManualReviewCountModel } from '../../../../state/identities/identities.selectors';
 import { useFilterUpdate } from '../../hooks/filterUpdate.hook';
 import { initialFilter } from '../../models/filter.model';
 import { useStyles } from './ManualReviewBanner.styles';
@@ -12,16 +13,25 @@ import { useStyles } from './ManualReviewBanner.styles';
 export const ManualReviewBanner = () => {
   const intl = useIntl();
   const classes = useStyles();
+  const dispatch = useDispatch();
   const manualReviewCount = useSelector(selectManualReviewCountModel);
+  const identityCount = useSelector(selectIdentityCountModel);
   const [setFilter, identityFilter] = useFilterUpdate();
   const [isFilterActive, setIsFilterActive] = useState(false);
+
+  useEffect(() => {
+    dispatch(identitiesManualReviewCountLoad());
+  }, [dispatch, identityCount]);
 
   useEffect(() => {
     setIsFilterActive(identityFilter.status.some((status) => status === IdentityStatuses.reviewNeeded));
   }, [identityFilter.status]);
 
   const handleFilterByManualReview = useCallback(() => {
-    setFilter({ ...initialFilter, status: [IdentityStatuses.reviewNeeded] });
+    setFilter({
+      ...initialFilter,
+      status: [IdentityStatuses.reviewNeeded],
+    });
     setIsFilterActive(true);
   }, [setFilter]);
 
@@ -35,38 +45,38 @@ export const ManualReviewBanner = () => {
   return (
     <>
       {manualReviewCount.value > 0 && (
-      <Grid item>
-        <Paper className={classes.banner}>
-          <Grid container justify="space-between" alignItems="center" className={classes.bannerWrapper}>
-            <Grid item>
-              <Typography variant="body1" className={classes.bannerText}>
-                {intl.formatMessage({
-                  id: 'VerificationHistory.manualReviewBanner',
-                }, {
-                  count: <b>{manualReviewCount.value}</b>,
-                })}
-              </Typography>
-            </Grid>
-            <Grid item>
-              <Button
-                variant="outlined"
-                className={classes.bannerButton}
-                onClick={handleFilterByManualReview}
-              >
-                {intl.formatMessage({ id: 'VerificationHistory.reviewVerifications' }) }
-              </Button>
-              {isFilterActive && (
+        <Grid item>
+          <Paper className={classes.banner}>
+            <Grid container justify="space-between" alignItems="center" className={classes.bannerWrapper}>
+              <Grid item>
+                <Typography variant="body1" className={classes.bannerText}>
+                  {intl.formatMessage({
+                    id: 'VerificationHistory.manualReviewBanner',
+                  }, {
+                    count: <b>{manualReviewCount.value}</b>,
+                  })}
+                </Typography>
+              </Grid>
+              <Grid item>
                 <Button
-                  onClick={handleClearManualReview}
-                  className={classes.bannerButtonClose}
+                  variant="outlined"
+                  className={classes.bannerButton}
+                  onClick={handleFilterByManualReview}
                 >
-                  <FiX />
+                  {intl.formatMessage({ id: 'VerificationHistory.reviewVerifications' })}
                 </Button>
-              )}
+                {isFilterActive && (
+                  <Button
+                    onClick={handleClearManualReview}
+                    className={classes.bannerButtonClose}
+                  >
+                    <FiX />
+                  </Button>
+                )}
+              </Grid>
             </Grid>
-          </Grid>
-        </Paper>
-      </Grid>
+          </Paper>
+        </Grid>
       )}
     </>
   );
