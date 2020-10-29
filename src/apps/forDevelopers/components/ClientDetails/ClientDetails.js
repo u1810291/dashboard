e467ab0eab38f4fdf1a50d9350f3c9a330634e4e
olyurmanova <ol.yurmanova@gmail.com>
2020-10-29 14:12:54 +0300

    start dev layout

--- src/apps/forDevelopers/components/ClientDetails/ClientDetails.js
+++ src/apps/forDevelopers/components/ClientDetails/ClientDetails.js
@@ -1,4 +1,4 @@
-import { IconButton, Typography } from '@material-ui/core';
+import { Box, IconButton, Typography } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import { VisibilityOff } from '@material-ui/icons';
@@ -11,45 +11,47 @@ export const ClientDetails = () => {
 
   return (
     <Paper>
-      <Grid container justify="space-between">
-        <Grid container xs={6}>
-          <Typography>
-            Client Details
-          </Typography>
-        </Grid>
-        <Grid container xs={6}>
-          <Grid container direction="column">
-            <Grid container>
-              <Typography>
-                {clientId}
-              </Typography>
-              <IconButton>
-                <VisibilityOff />
-              </IconButton>
+      <Box p={2}>
+        <Grid container spacing={2} justify="space-between">
+          <Grid item xs={6}>
+            <Typography>
+              Client Details
+            </Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Grid container direction="column">
+              <Grid container>
+                <Typography>
+                  {clientId}
+                </Typography>
+                <IconButton>
+                  <VisibilityOff />
+                </IconButton>
+              </Grid>
+              <Grid item>
+                <Typography>Client id</Typography>
+              </Grid>
             </Grid>
-            <Grid item>
-              <Typography>Client id</Typography>
+            <Grid container direction="column">
+              <Grid container>
+                <Typography>5e9576d8ac2c70001ca9ee3d</Typography>
+                <IconButton>
+                  <VisibilityOff />
+                </IconButton>
+                <IconButton>
+                  <VisibilityOff />
+                </IconButton>
+              </Grid>
+              <Grid container>
+                <Typography>Client secret</Typography>
+                <IconButton>
+                  <VisibilityOff />
+                </IconButton>
+              </Grid>
             </Grid>
           </Grid>
-          <Grid container direction="column">
-            <Grid container>
-              <Typography>5e9576d8ac2c70001ca9ee3d</Typography>
-              <IconButton>
-                <VisibilityOff />
-              </IconButton>
-              <IconButton>
-                <VisibilityOff />
-              </IconButton>
-            </Grid>
-            <Grid container>
-              <Typography>Client secret</Typography>
-              <IconButton>
-                <VisibilityOff />
-              </IconButton>
-            </Grid>
-          </Grid>
         </Grid>
-      </Grid>
+      </Box>
     </Paper>
   );
 };
--- src/apps/forDevelopers/containers/ForDev/ForDevs.js
+++ src/apps/forDevelopers/containers/ForDev/ForDevs.js
@@ -1,4 +1,4 @@
-import { Container, FormControl, Typography } from '@material-ui/core';
+import { Box, Container, FormControl, Typography } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import MenuItem from '@material-ui/core/MenuItem';
@@ -33,53 +33,57 @@ export const ForDevs = () => {
 
   return (
     <Container>
-      <ClientDetails />
-      <Paper>
-        <Grid container direction="column">
-          <Grid item>
-            Integrations
-          </Grid>
-          <Grid container direction="row">
-            <Grid item xs={3}>
-              <Typography>Flow</Typography>
-              <FormControl variant="outlined">
-                <Select
-                  labelId="demo-customized-select-label"
-                  id="demo-customized-select"
-                  onChange={handleSelectedFlow}
-                >
-                  {!LoadableAdapter.isPristine(merchantFlowList) && merchantFlowList.value.map((item) => (
-                    <MenuItem
-                      key={item.id}
-                      value={item.id}
-                      checked={selectedFlow === item.id}
-                      control={<Radio color="default" checkedIcon={<RadioOn />} icon={<RadioOff />} />}
+      <Box pt={{ xs: 2, lg: 4 }}>
+        <ClientDetails />
+        <Paper>
+          <Box p={2}>
+            <Grid container direction="column">
+              <Grid item>
+                Integrations
+              </Grid>
+              <Grid container direction="row">
+                <Grid item xs={3}>
+                  <Typography>Flow</Typography>
+                  <FormControl variant="outlined">
+                    <Select
+                      labelId="demo-customized-select-label"
+                      id="demo-customized-select"
+                      onChange={handleSelectedFlow}
                     >
-                      {item.name}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
+                      {!LoadableAdapter.isPristine(merchantFlowList) && merchantFlowList.value.map((item) => (
+                        <MenuItem
+                          key={item.id}
+                          value={item.id}
+                          checked={selectedFlow === item.id}
+                          control={<Radio color="default" checkedIcon={<RadioOn />} icon={<RadioOff />} />}
+                        >
+                          {item.name}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                </Grid>
+                <Grid item xs={3}>
+                  <Typography>Active</Typography>
+                  <Typography> Webhook status</Typography>
+                </Grid>
+                <Button>
+                  <SettingsIcon />
+                  Webhooks
+                </Button>
+                <Button>Mati Documentation</Button>
+              </Grid>
             </Grid>
-            <Grid item xs={3}>
-              <Typography>Active</Typography>
-              <Typography> Webhook status</Typography>
+            <Grid container>
+              <Grid container direction="column" xs={3}>
+                <TabsMenu onClick={handleTabChange} />
+              </Grid>
+              {selectedTab === TabID.API && (<Grid>API</Grid>)}
+              {selectedTab === TabID.DIRECT_LINK && (<Grid>Direct Link</Grid>)}
             </Grid>
-            <Button>
-              <SettingsIcon />
-              Webhooks
-            </Button>
-            <Button>Mati Documentation</Button>
-          </Grid>
-        </Grid>
-        <Grid container>
-          <Grid container direction="column" xs={3}>
-            <TabsMenu onClick={handleTabChange} />
-          </Grid>
-          {selectedTab === TabID.API && (<Grid>API</Grid>)}
-          {selectedTab === TabID.DIRECT_LINK && (<Grid>Direct Link</Grid>)}
-        </Grid>
-      </Paper>
+          </Box>
+        </Paper>
+      </Box>
     </Container>
   );
 };
