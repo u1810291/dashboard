a4016b0449adfec4f120138f7e72b832bd3d09e6
olyurmanova <ol.yurmanova@gmail.com>
2020-11-03 14:58:11 +0300

    InformationPage layout fix

--- src/apps/forDevelopers/components/InformationPage/InformationPage.js
+++ src/apps/forDevelopers/components/InformationPage/InformationPage.js
@@ -1,5 +1,4 @@
-import { Typography } from '@material-ui/core';
-import Grid from '@material-ui/core/Grid';
+import { Typography, Box } from '@material-ui/core';
 import React, { useCallback } from 'react';
 import { useIntl } from 'react-intl';
 import { TabID } from '../../../../models/ForDevelopers.model';
@@ -53,6 +52,7 @@ export const InformationPage = ({ selectedPage }) => {
       id: TabID.IOS,
       name: intl.formatMessage({ id: 'forDevs.sideMenu.ios' }),
       header: 'Native MobileSDK iOS Integration',
+      subHeader: intl.formatMessage({ id: 'forDevs.informationPage.mobile.subheader' }),
       image: '',
       videoURL: 'https://media.w3.org/2010/05/sintel/trailer.mp4',
       documentationURL: 'https://github.com',
@@ -61,6 +61,7 @@ export const InformationPage = ({ selectedPage }) => {
       id: TabID.REACT_NATIVE,
       name: intl.formatMessage({ id: 'forDevs.sideMenu.reactNative' }),
       header: 'Hybrid React Native Integration',
+      subHeader: intl.formatMessage({ id: 'forDevs.informationPage.mobile.subheader' }),
       videoURL: 'https://media.w3.org/2010/05/sintel/trailer.mp4',
       documentationURL: 'https://github.com',
     },
@@ -68,6 +69,7 @@ export const InformationPage = ({ selectedPage }) => {
       id: TabID.XAMARIN,
       name: intl.formatMessage({ id: 'forDevs.sideMenu.xamarin' }),
       header: 'Hybrid Xamarin Integration',
+      subHeader: intl.formatMessage({ id: 'forDevs.informationPage.mobile.subheader' }),
       videoURL: 'https://media.w3.org/2010/05/sintel/trailer.mp4',
       documentationURL: 'https://github.com',
     },
@@ -75,6 +77,7 @@ export const InformationPage = ({ selectedPage }) => {
       id: TabID.CORDOVA,
       name: intl.formatMessage({ id: 'forDevs.sideMenu.cordova' }),
       header: 'Hybrid Cordova Integration',
+      subHeader: intl.formatMessage({ id: 'forDevs.informationPage.mobile.subheader' }),
       videoURL: 'https://media.w3.org/2010/05/sintel/trailer.mp4',
       documentationURL: 'https://github.com',
     },
@@ -82,6 +85,7 @@ export const InformationPage = ({ selectedPage }) => {
       id: TabID.CORDOVA_IONIC,
       name: intl.formatMessage({ id: 'forDevs.sideMenu.cordovaIonic' }),
       header: 'Hybrid Cordova ionic Integration',
+      subHeader: intl.formatMessage({ id: 'forDevs.informationPage.mobile.subheader' }),
       videoURL: 'https://media.w3.org/2010/05/sintel/trailer.mp4',
       documentationURL: 'https://github.com',
     },
@@ -89,21 +93,27 @@ export const InformationPage = ({ selectedPage }) => {
 
   return (
     <>
-      {pagesData().map(({ id, name, header, videoURL, documentationURL, image, subheader, childComponent }) => (
+      {pagesData().map(({ id, name, header, videoURL, documentationURL, image, subHeader, childComponent }) => (
         <>
           { id === selectedPage && (
-            <Grid container>
-              <GithubDocumentationBanner documentationURL={documentationURL} platform={name} />
-              <Grid item>
-                <Typography>{header}</Typography>
-                <Typography>
-                  {subheader}
-                </Typography>
+            <Box>
+              <Box mb={4}>
+                <GithubDocumentationBanner documentationURL={documentationURL} platform={name} />
+              </Box>
+              <Typography variant="subtitle2" gutterBottom>{header}</Typography>
+              <Box mb={2} color="common.black75">{subHeader}</Box>
+              <Box mb={4}>
                 <img src={image} alt="sdk" />
-                {childComponent}
+              </Box>
+              {childComponent && (
+                <Box pt={4} mb={4} borderTop={1} borderColor="common.black7">
+                  {childComponent}
+                </Box>
+              )}
+              <Box pt={4} borderTop={1} borderColor="common.black7">
                 <VideoExplainer videoURL={videoURL} name={name} />
-              </Grid>
-            </Grid>
+              </Box>
+            </Box>
           )}
         </>
       ))}
