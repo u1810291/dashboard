28cb1c987ec0abadfc522d40125b72ca622fd8c2
Vladislav Snimshchikov <vsnimshchikov@emphasoft.com>
2020-11-03 16:31:04 +0300

    text information added

--- src/apps/forDevelopers/components/InformationPage/DirectLinkCopy.js
+++ src/apps/forDevelopers/components/InformationPage/DirectLinkCopy.js
@@ -2,6 +2,7 @@ import { Typography } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import React from 'react';
 import { useIntl } from 'react-intl';
+import { CopyToClipboard } from '../../../../components/clipboard';
 
 export const DirectLinkCopy = () => {
   const intl = useIntl();
@@ -9,6 +10,11 @@ export const DirectLinkCopy = () => {
     <Grid container direction="column">
       <Typography>{intl.formatMessage({ id: 'forDevs.directLinkCopy.header' })}</Typography>
       <Typography>{intl.formatMessage({ id: 'forDevs.directLinkCopy.subheader' })}</Typography>
+      <CopyToClipboard text="code">
+        <code>
+          code
+        </code>
+      </CopyToClipboard>
     </Grid>
   );
 };
--- src/apps/forDevelopers/components/InformationPage/InformationPage.js
+++ src/apps/forDevelopers/components/InformationPage/InformationPage.js
@@ -1,4 +1,4 @@
-import { Typography, Box } from '@material-ui/core';
+import { Box, Typography } from '@material-ui/core';
 import React, { useCallback } from 'react';
 import { useIntl } from 'react-intl';
 import { TabID } from '../../../../models/ForDevelopers.model';
@@ -9,87 +9,108 @@ import { WebCopy } from './WebCopy';
 
 export const InformationPage = ({ selectedPage }) => {
   const intl = useIntl();
-  const pagesData = useCallback(() => ([
-    {
-      id: TabID.API,
-      name: intl.formatMessage({ id: 'forDevs.sideMenu.api' }),
-      header: `${intl.formatMessage({ id: 'forDevs.sideMenu.api' })} ${intl.formatMessage({ id: 'forDevs.integration' })}`,
-      subHeader: intl.formatMessage({ id: 'forDevs.informationPage.api.subheader' }),
-      image: '',
-      videoURL: 'https://media.w3.org/2010/05/sintel/trailer.mp4',
-      documentationURL: 'https://github.com',
-    },
-    {
-      id: TabID.WEB,
-      name: intl.formatMessage({ id: 'forDevs.sideMenu.web' }),
-      header: `${intl.formatMessage({ id: 'forDevs.sideMenu.web' })} ${intl.formatMessage({ id: 'forDevs.integration' })}`,
-      subHeader: intl.formatMessage({ id: 'forDevs.informationPage.web.subheader' }),
-      image: '',
-      videoURL: 'https://media.w3.org/2010/05/sintel/trailer.mp4',
-      documentationURL: 'https://github.com',
-      childComponent: (<WebCopy />),
-    },
-    {
-      id: TabID.DIRECT_LINK,
-      name: intl.formatMessage({ id: 'forDevs.sideMenu.directLink' }),
-      header: intl.formatMessage({ id: 'forDevs.sideMenu.directLink' }),
-      subHeader: intl.formatMessage({ id: 'forDevs.informationPage.directLink.subheader' }),
-      image: '',
-      videoURL: 'https://media.w3.org/2010/05/sintel/trailer.mp4',
-      documentationURL: 'https://github.com',
-      childComponent: (<DirectLinkCopy />),
-    },
-    {
-      id: TabID.ANDROID,
-      name: intl.formatMessage({ id: 'forDevs.sideMenu.android' }),
-      header: 'Native MobileSDK Android Integration',
-      subHeader: intl.formatMessage({ id: 'forDevs.informationPage.mobile.subheader' }),
-      image: '',
-      videoURL: 'https://media.w3.org/2010/05/sintel/trailer.mp4',
-      documentationURL: 'https://github.com',
-    },
-    {
-      id: TabID.IOS,
-      name: intl.formatMessage({ id: 'forDevs.sideMenu.ios' }),
-      header: 'Native MobileSDK iOS Integration',
-      subHeader: intl.formatMessage({ id: 'forDevs.informationPage.mobile.subheader' }),
-      image: '',
-      videoURL: 'https://media.w3.org/2010/05/sintel/trailer.mp4',
-      documentationURL: 'https://github.com',
-    },
-    {
-      id: TabID.REACT_NATIVE,
-      name: intl.formatMessage({ id: 'forDevs.sideMenu.reactNative' }),
-      header: 'Hybrid React Native Integration',
-      subHeader: intl.formatMessage({ id: 'forDevs.informationPage.mobile.subheader' }),
-      videoURL: 'https://media.w3.org/2010/05/sintel/trailer.mp4',
-      documentationURL: 'https://github.com',
-    },
-    {
-      id: TabID.XAMARIN,
-      name: intl.formatMessage({ id: 'forDevs.sideMenu.xamarin' }),
-      header: 'Hybrid Xamarin Integration',
-      subHeader: intl.formatMessage({ id: 'forDevs.informationPage.mobile.subheader' }),
-      videoURL: 'https://media.w3.org/2010/05/sintel/trailer.mp4',
-      documentationURL: 'https://github.com',
-    },
-    {
-      id: TabID.CORDOVA,
-      name: intl.formatMessage({ id: 'forDevs.sideMenu.cordova' }),
-      header: 'Hybrid Cordova Integration',
-      subHeader: intl.formatMessage({ id: 'forDevs.informationPage.mobile.subheader' }),
-      videoURL: 'https://media.w3.org/2010/05/sintel/trailer.mp4',
-      documentationURL: 'https://github.com',
-    },
-    {
-      id: TabID.CORDOVA_IONIC,
-      name: intl.formatMessage({ id: 'forDevs.sideMenu.cordovaIonic' }),
-      header: 'Hybrid Cordova ionic Integration',
-      subHeader: intl.formatMessage({ id: 'forDevs.informationPage.mobile.subheader' }),
-      videoURL: 'https://media.w3.org/2010/05/sintel/trailer.mp4',
-      documentationURL: 'https://github.com',
-    },
-  ]), [intl]);
+  const pagesData = useCallback(() => {
+    const integration = intl.formatMessage({ id: 'forDevs.integration' });
+    return [
+      {
+        id: TabID.API,
+        name: intl.formatMessage({ id: 'forDevs.sideMenu.api' }),
+        header: `${intl.formatMessage({ id: 'forDevs.sideMenu.api' })} ${integration}`,
+        subHeader: intl.formatMessage(
+          { id: 'forDevs.informationPage.api.subheader' },
+          { name: `${intl.formatMessage({ id: 'forDevs.sideMenu.api' })} ${integration}` }),
+        image: '',
+        videoURL: 'https://media.w3.org/2010/05/sintel/trailer.mp4',
+        documentationURL: 'https://github.com',
+      },
+      {
+        id: TabID.WEB,
+        name: intl.formatMessage({ id: 'forDevs.sideMenu.web' }),
+        header: `${intl.formatMessage({ id: 'forDevs.sideMenu.web' })} ${integration}`,
+        subHeader: intl.formatMessage(
+          { id: 'forDevs.informationPage.web.subheader' },
+          { name: `${intl.formatMessage({ id: 'forDevs.sideMenu.web' })} ${integration}` }),
+        image: '',
+        videoURL: 'https://media.w3.org/2010/05/sintel/trailer.mp4',
+        documentationURL: 'https://github.com',
+        childComponent: (<WebCopy />),
+      },
+      {
+        id: TabID.DIRECT_LINK,
+        name: intl.formatMessage({ id: 'forDevs.sideMenu.directLink' }),
+        header: intl.formatMessage({ id: 'forDevs.sideMenu.directLink' }),
+        subHeader: intl.formatMessage(
+          { id: 'forDevs.informationPage.directLink.subheader' },
+          { name: intl.formatMessage({ id: 'forDevs.sideMenu.directLink' }) }),
+        image: '',
+        videoURL: 'https://media.w3.org/2010/05/sintel/trailer.mp4',
+        documentationURL: 'https://github.com',
+        childComponent: (<DirectLinkCopy />),
+      },
+      {
+        id: TabID.ANDROID,
+        name: intl.formatMessage({ id: 'forDevs.sideMenu.android' }),
+        header: `${intl.formatMessage({ id: 'forDevs.sideMenu.android' })} ${integration}`,
+        subHeader: intl.formatMessage(
+          { id: 'forDevs.informationPage.mobile.subheader' },
+          { name: `${intl.formatMessage({ id: 'forDevs.sideMenu.android' })} ${integration}` }),
+        image: '',
+        videoURL: 'https://media.w3.org/2010/05/sintel/trailer.mp4',
+        documentationURL: 'https://github.com',
+      },
+      {
+        id: TabID.IOS,
+        name: intl.formatMessage({ id: 'forDevs.sideMenu.ios' }),
+        header: `${intl.formatMessage({ id: 'forDevs.sideMenu.ios' })} ${integration}`,
+        subHeader: intl.formatMessage(
+          { id: 'forDevs.informationPage.mobile.subheader' },
+          { name: `${intl.formatMessage({ id: 'forDevs.sideMenu.ios' })} ${integration}` }),
+        image: '',
+        videoURL: 'https://media.w3.org/2010/05/sintel/trailer.mp4',
+        documentationURL: 'https://github.com',
+      },
+      {
+        id: TabID.REACT_NATIVE,
+        name: intl.formatMessage({ id: 'forDevs.sideMenu.reactNative' }),
+        header: `${intl.formatMessage({ id: 'forDevs.sideMenu.reactNative' })} ${integration}`,
+        subHeader: intl.formatMessage(
+          { id: 'forDevs.informationPage.mobile.subheader' },
+          { name: `${intl.formatMessage({ id: 'forDevs.sideMenu.reactNative' })} ${integration}` }),
+        videoURL: 'https://media.w3.org/2010/05/sintel/trailer.mp4',
+        documentationURL: 'https://github.com',
+      },
+      {
+        id: TabID.XAMARIN,
+        name: intl.formatMessage({ id: 'forDevs.sideMenu.xamarin' }),
+        header: `${intl.formatMessage({ id: 'forDevs.sideMenu.xamarin' })} ${integration}`,
+        subHeader: intl.formatMessage(
+          { id: 'forDevs.informationPage.mobile.subheader' },
+          { name: `${intl.formatMessage({ id: 'forDevs.sideMenu.xamarin' })} ${integration}` }),
+        videoURL: 'https://media.w3.org/2010/05/sintel/trailer.mp4',
+        documentationURL: 'https://github.com',
+      },
+      {
+        id: TabID.CORDOVA,
+        name: intl.formatMessage({ id: 'forDevs.sideMenu.cordova' }),
+        header: `${intl.formatMessage({ id: 'forDevs.sideMenu.cordova' })} ${integration}`,
+        subHeader: intl.formatMessage(
+          { id: 'forDevs.informationPage.mobile.subheader' },
+          { name: `${intl.formatMessage({ id: 'forDevs.sideMenu.cordova' })} ${integration}` }),
+        videoURL: 'https://media.w3.org/2010/05/sintel/trailer.mp4',
+        documentationURL: 'https://github.com',
+      },
+      {
+        id: TabID.CORDOVA_IONIC,
+        name: intl.formatMessage({ id: 'forDevs.sideMenu.cordovaIonic' }),
+        header: `${intl.formatMessage({ id: 'forDevs.sideMenu.cordovaIonic' })} ${integration}`,
+        subHeader: intl.formatMessage(
+          { id: 'forDevs.informationPage.mobile.subheader' },
+          { name: `${intl.formatMessage({ id: 'forDevs.sideMenu.cordovaIonic' })} ${integration}` }),
+        videoURL: 'https://media.w3.org/2010/05/sintel/trailer.mp4',
+        documentationURL: 'https://github.com',
+      },
+    ];
+  }, [intl]);
 
   return (
     <>
