9c4ccb3b04de4144f6989a8938697f127731ceba
Vladislav Snimshchikov <vsnimshchikov@emphasoft.com>
2020-11-03 10:42:32 +0300

    selected class for cascade button added

--- src/apps/forDevelopers/components/CascadeMenuButton/CascadeMenuButton.js
+++ src/apps/forDevelopers/components/CascadeMenuButton/CascadeMenuButton.js
@@ -2,17 +2,18 @@ import { Collapse } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import React, { useCallback, useState } from 'react';
 import Button from '../../../../components/button';
+import { useStyles } from './CascadeMenuButton.styles';
 
 export const CascadeMenuButton = ({ name, children }) => {
   const [isOpen, setIsOpen] = useState(false);
-
+  const classes = useStyles();
   const handleOnClick = useCallback(() => {
     setIsOpen(((prevState) => !prevState));
   }, []);
 
   return (
     <Grid item direction="column">
-      <Button onClick={handleOnClick}>{name}</Button>
+      <Button className={classes.selected} onClick={handleOnClick}>{name}</Button>
       <Collapse in={isOpen}>
         <Grid container direction="column">
           {children}
