6c2f0b4ebfdbf1872e0dd6046f86379f2b0aad85
Eugene Butusov <evgeny.butusov@mati.io>
2021-01-27 19:35:58 +0300

    PGS-122 Remove `can-use-duplicate-user-detection` merchant tag

--- src/models/Merchant.model.js
+++ src/models/Merchant.model.js
@@ -1,6 +1,5 @@
 export const MerchantTags = {
   CanUseLongPolling: 'can-use-long-polling',
   CanUseNom151: 'can-use-nom-151',
-  CanUseDuplicateUserDetection: 'can-use-duplicate-user-detection',
   CanUseComplyAdvantageIntegratedCheck: 'can-use-comply-advantage',
 };
