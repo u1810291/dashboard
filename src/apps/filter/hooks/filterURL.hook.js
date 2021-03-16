2885ccbf5e05cfd676bfe9d6db22699b082bb9d8
rashad.ibrahimov <rashad.ibrahimov@mati.io>
2021-03-16 11:10:27 +0000

    release/dio-2021.02.2

--- src/apps/filter/hooks/filterURL.hook.js
+++ src/apps/filter/hooks/filterURL.hook.js
@@ -1,13 +1,13 @@
-import { filterSerialize } from 'models/Filter.model';
+import { filterSerialize, parseFromURL } from 'models/Filter.model';
 import { useCallback } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 
-export function useFilterParser() {
+export function useFilterParser(filterStructure) {
   const history = useHistory();
   const location = useLocation();
 
-  const updateURL = useCallback((filter) => {
-    const newParams = filterSerialize({ ...filter });
+  const setURLFromFilter = useCallback((filter) => {
+    const newParams = filterSerialize(filter);
     const search = new URLSearchParams(newParams);
     history.push({
       pathname: location.pathname,
@@ -15,5 +15,16 @@ export function useFilterParser() {
     });
   }, [history, location.pathname]);
 
-  return [updateURL];
+  const addToUrl = useCallback((filter) => {
+    const currentParams = parseFromURL(location.search || '', filterStructure);
+    const newParams = { ...currentParams, ...filter };
+    const newFilter = filterSerialize(newParams);
+    const search = new URLSearchParams(newFilter);
+    history.push({
+      pathname: location.pathname,
+      search: search.toString(),
+    });
+  }, [filterStructure, history, location.pathname, location.search]);
+
+  return [setURLFromFilter, addToUrl];
 }
