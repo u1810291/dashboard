fe2b947bdb770c3ca1743eb801cdbad171b346a9
sergei.kharitonov <sergei.kharitonov@mati.io>
2021-07-08 13:03:59 +0000

    ATL-359 Merchant tag + stub page for Phone validation

--- src/apps/PhoneValidation/state/PhoneValidation.selectors.ts
+++ src/apps/PhoneValidation/state/PhoneValidation.selectors.ts
@@ -1,6 +1,7 @@
 import { VerificationPatternTypes } from 'models/VerificationPatterns.model';
 import { createSelector } from 'reselect';
-import { selectCurrentFlow, selectVerificationPattern } from 'state/merchant/merchant.selectors';
+import { selectCurrentFlow, selectVerificationPattern, selectMerchantTags } from 'state/merchant/merchant.selectors';
+import { MerchantTags } from '../../../models/Merchant.model';
 
 export const selectPhoneOwnership = createSelector(
   selectCurrentFlow,
@@ -15,4 +16,9 @@ export const selectSenderName = createSelector(
 export const selectPhoneValidationMode = createSelector(
   selectVerificationPattern,
   (patterns) => patterns[VerificationPatternTypes.PhoneOwnershipValidation],
+);
+
+export const selectCanUsePhoneValidation = createSelector(
+  selectMerchantTags,
+  (tags: MerchantTags[]): boolean => tags.includes(MerchantTags.CanUsePhoneValidation),
 );
---
+++ src/apps/merchant/ProductCanUseContainer/ProductCanUseContainer.styles.tsx
@@ -0,0 +1,8 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const useStyles = makeStyles(() => ({
+  container: {
+    pointerEvents: 'none',
+    opacity: 0.5,
+  },
+}));
