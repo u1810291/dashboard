52a44758569329ce42a1eeb528b5678035d21a0a
vladislav.snimshchikov <vladislav.snimshchikov@mati.io>
2021-09-06 14:21:42 +0000

    fix DIO-609 "Auth token for images"

--- src/apps/media/api/media.client.ts
+++ src/apps/media/api/media.client.ts
@@ -1,6 +1,7 @@
 import { http } from 'lib/client/http';
+import { ClientMethodTypes } from 'models/Client.model';
 
-export function getMedia(src) {
-  const { headers } = http.getAuthHeader();
+export async function getMedia(src) {
+  const headers = await http.getHeaders(ClientMethodTypes.GET, true);
   return fetch(src, { headers });
 }
