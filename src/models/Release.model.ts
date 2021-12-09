6b8f86e34af99a09753e1fffe7caa1004b905fad
vladislav.snimshchikov <vladislav.snimshchikov@mati.io>
2021-12-09 16:23:12 +0300

    enable identity profile

--- src/models/Release.model.ts
+++ src/models/Release.model.ts
@@ -2,4 +2,4 @@
 export const IS_FLOW_BUILDER_RELEASED = true;
 export const REDUCE_DB_COUNT_CALLS = true;
 export const IS_IDENTITY_PROFILE_RELEASED = true;
-export const IS_REPLACE_PROFILE_TO_IDENTITY = true;
+export const IS_REPLACE_PROFILE_TO_IDENTITY = false;
