e897d179e0259a47303a07018f95b6a9e05a7a26
vladislav.snimshchikov <vladislav.snimshchikov@mati.io>
2022-01-17 14:11:15 +0300

    show json with quotes

--- src/lib/stringify.js
+++ src/lib/stringify.js
@@ -1,4 +1,7 @@
-export default function stringify(object, indent = 2) {
-  const string = JSON.stringify(object, null, indent);
-  return string.replace(/"([^(")"]+)":/g, '$1:');
+export default function stringify(object, indent = 2, isWithQuotes = false) {
+  if (!object) {
+    return '';
+  }
+  const result = JSON.stringify(object, null, indent);
+  return isWithQuotes ? result : result.replace(/"([^(")"]+)":/g, '$1:');
 }
