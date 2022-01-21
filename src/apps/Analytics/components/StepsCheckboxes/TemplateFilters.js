26c59c450ca2c2df74abf2d817aadf03b9b44f04
kirill.goriachkin <kirill.goriachkin@mati.io>
2022-01-21 11:02:00 +0300

    fixed onClickOutside with mui onclickoutside

--- src/apps/Analytics/components/StepsCheckboxes/TemplateFilters.js
+++ src/apps/Analytics/components/StepsCheckboxes/TemplateFilters.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useState } from 'react';
 import { Box, Checkbox, FormControlLabel, Paper } from '@material-ui/core';
-import listenForOutsideClick from './hooks/listenForOutsideClicks.hook';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 import { ReactComponent as CheckboxOn } from 'assets/icon-checkbox-on.svg';
 import { ReactComponent as CheckboxOff } from 'assets/icon-checkbox-off.svg';
 import FilterListIcon from '@material-ui/icons/FilterList';
@@ -27,36 +27,35 @@ const MOCK_DATA = [
 
 export function TemplateFilters({ buttonTitle }) {
   const classes = useStyles();
-  const boxRef = useRef(null);
-  const [listening, setListening] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
-
-  useEffect(listenForOutsideClick(listening, setListening, boxRef, setIsOpen));
+  const handleClickAway = () => setIsOpen(false);
 
   return (
-    <Box mb={2} className={classes.filterContainer} ref={boxRef}>
-      <FilterButton
-        onClick={toggle}
-        variant="contained"
-        startIcon={<FilterListIcon />}
-      >
-        <Box component="span" className={classes.buttonName}>{buttonTitle}</Box>
-      </FilterButton>
-      {isOpen
-      && (
-        <Paper className={classes.filterOptions}>
-          {MOCK_DATA.length > 0 && MOCK_DATA.map((item, idx) => (
-            <FormControlLabel
-              className={classes.filterOption}
-              key={idx}
-              value={item}
-              control={<Checkbox color="primary" defaultChecked={item === 'All' && true} checkedIcon={<CheckboxOn />} icon={<CheckboxOff />} />}
-              label={item}
-            />
-          ))}
-        </Paper>
-      )}
-    </Box>
+    <ClickAwayListener onClickAway={handleClickAway}>
+      <Box mb={2} className={classes.filterContainer}>
+        <FilterButton
+          onClick={toggle}
+          variant="contained"
+          startIcon={<FilterListIcon />}
+        >
+          <Box component="span" className={classes.buttonName}>{buttonTitle}</Box>
+        </FilterButton>
+        {isOpen
+        && (
+          <Paper className={classes.filterOptions}>
+            {MOCK_DATA.length > 0 && MOCK_DATA.map((item, idx) => (
+              <FormControlLabel
+                className={classes.filterOption}
+                key={idx}
+                value={item}
+                control={<Checkbox color="primary" defaultChecked={item === 'All' && true} checkedIcon={<CheckboxOn />} icon={<CheckboxOff />} />}
+                label={item}
+              />
+            ))}
+          </Paper>
+        )}
+      </Box>
+    </ClickAwayListener>
   );
 }
