df3f8e4e568fdd224af37b2896605f807bba4f22
Oleg Popov <dearenot@gmail.com>
2022-02-01 18:38:19 +0300

    remove comments from s&p template

--- src/apps/Templates/components/SaveAndPublishTemplate/SaveAndPublishTemplate.tsx
+++ src/apps/Templates/components/SaveAndPublishTemplate/SaveAndPublishTemplate.tsx
@@ -1,43 +1,22 @@
 import { Button, Grid } from '@material-ui/core';
 import { selectFlowBuilderHaveUnsavedChanges, selectFlowBuilderProductsInGraphModel } from 'apps/flowBuilder/store/FlowBuilder.selectors';
 import { useProductsIssues } from 'apps/Product/hooks/ProductIssues.hook';
-import { notification, TextBubble } from 'apps/ui';
+import { TextBubble } from 'apps/ui';
 import { useOverlay } from 'apps/overlay';
-import React, { useCallback, useEffect } from 'react';
+import React from 'react';
 import { FiSave } from 'react-icons/fi';
 import { useIntl } from 'react-intl';
-import { useDispatch, useSelector } from 'react-redux';
-// import { flowBuilderSaveAndPublish } from '../../store/FlowBuilder.action';
-import { useStyles } from './SaveAndPublishTemplate.style';
+import { useSelector } from 'react-redux';
 import { TemplateSaveModal } from 'apps/Templates/components/TemplateSaveModal/TemplateSaveModal';
+import { useStyles } from './SaveAndPublishTemplate.style';
 
 export function SaveAndPublishTemplate() {
   const classes = useStyles();
   const intl = useIntl();
-  const dispatch = useDispatch();
   const productsInGraphModel = useSelector(selectFlowBuilderProductsInGraphModel);
   const haveUnsavedChanges = useSelector(selectFlowBuilderHaveUnsavedChanges);
   const haveIssues = useProductsIssues(productsInGraphModel.value);
   const [createOverlay] = useOverlay();
-
-  console.log('iss ', haveIssues);
-
-  // useEffect(() => {
-  //   function handleBeforeUnload(event) {
-  //     event.preventDefault();
-  //     // eslint-disable-next-line no-param-reassign
-  //     event.returnValue = '';
-  //   }
-
-  //   if (haveUnsavedChanges) {
-  //     window.addEventListener('beforeunload', handleBeforeUnload);
-  //   } else {
-  //     window.removeEventListener('beforeunload', handleBeforeUnload);
-  //   }
-  //   return () => {
-  //     window.removeEventListener('beforeunload', handleBeforeUnload);
-  //   };
-  // }, [haveUnsavedChanges]);
 
   const handleSaveFlow = () => {
     createOverlay(<TemplateSaveModal onSubmit={() => {}} />);
