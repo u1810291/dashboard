2f51060bcf913090b1ca355a5af317e8f0977e3d
kirill.goriachkin <kirill.goriachkin@mati.io>
2022-02-07 12:14:54 +0300

    added types for SolutionCatalog.client

--- src/apps/SolutionCatalog/api/SolutionCatalog.client.ts
+++ src/apps/SolutionCatalog/api/SolutionCatalog.client.ts
@@ -1,5 +1,6 @@
 import { http } from 'lib/client/http';
+import { CardsOptions, TemplateFilterOptions } from 'apps/SolutionCatalog';
 
-export function getTemplates(params) {
-  return http.get('/api/v1/dashboard/template', { params });
+export function getTemplates(params: TemplateFilterOptions[]) {
+  return http.get<Record<string, CardsOptions[]>>('/api/v1/dashboard/template', { params });
 }
