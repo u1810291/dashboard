6e6378a12c8f611e78a56c68003f50b086897925
Oleg Popov <dearenot@gmail.com>
2022-02-07 13:54:28 +0300

    fix imports
    	изменено:      src/apps/Templates/components/TemplateBuilder/TemplateBuilder.tsx
    	изменено:      src/apps/flowBuilder/index.ts

--- src/apps/flowBuilder/index.ts
+++ src/apps/flowBuilder/index.ts
@@ -1,3 +1,9 @@
 export { FlowBuilder } from './components/FlowBuilder/FlowBuilder';
 
 export * from './services/ProductBaseFlowBuilder.service';
+export { dagreGraphService } from './services/dagreGraph.service';
+export { FlowInfoContainer } from './components/FlowInfoContainer/FlowInfoContainer';
+export { FlowProductsGraph } from './components/FlowProductsGraph/FlowProductsGraph';
+export { ProductListSidebar } from './components/ProductListSidebar/ProductListSidebar';
+export { FlowBuilderProductDetails } from './components/FlowBuilderProductDetails/FlowBuilderProductDetails';
+export { FlowBuilderIntegrationDetails } from './components/FlowBuilderIntegrationDetails/FlowBuilderIntegrationDetails';
