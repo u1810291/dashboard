62fd8aa5d673225609c7843ec7909f5728d2eaa4
kirill.goriachkin <kirill.goriachkin@mati.io>
2022-03-03 22:05:45 +0300

    fixed goBack button for templates

--- src/apps/flowBuilder/components/FlowInfoContainer/FlowInfoContainer.tsx
+++ src/apps/flowBuilder/components/FlowInfoContainer/FlowInfoContainer.tsx
@@ -11,9 +11,10 @@ import { useStyles } from './FlowInfoContainer.styles';
 
 export function FlowInfoContainer({ isTemplate }: { isTemplate?: boolean }) {
   const classes = useStyles();
+  const route = isTemplate ? 'templates' : 'flow';
   const [createOverlay, closeOverlay] = useOverlay();
   const location = useLocation();
-  const goBackToListLink = useMemo(() => getGoBackToListLink(location, Routes.flow.root), [location]);
+  const goBackToListLink = useMemo(() => getGoBackToListLink(location, Routes[route].root), [location]);
   const handleOpenFlowSettings = useCallback(() => {
     createOverlay(<Modal className={classes.modal} onClose={closeOverlay}><FlowSettings isTemplate={isTemplate} onClose={closeOverlay} /></Modal>);
   }, [classes.modal, closeOverlay, createOverlay, isTemplate]);
