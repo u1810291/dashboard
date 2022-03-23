fff83ec028a085687e61346f3b34621e020e85c4
kairat.dzhumadylov <kairat.dzhumadylov@mati.io>
2022-03-23 14:20:42 +0600

    lokalize update

--- src/apps/CustomField/index.tsx
+++ src/apps/CustomField/index.tsx
@@ -4,4 +4,3 @@ export { CustomFieldInit } from './state/CustomField.actions';
 export { CUSTOM_FIELD_STORE_KEY } from './state/CustomField.store';
 export { customFieldReducer } from './state/CustomField.reducer';
 export { CustomFieldVerificationAtomicList, CustomFieldVerificationList } from './components/CustomFieldVerificationList/CustomFieldVerificationList';
-
