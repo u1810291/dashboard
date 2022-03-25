5093cbb72dc7bdad2e95ce914e67e7e7dcc44437
vladislav.snimshchikov <vladislav.snimshchikov@mati.io>
2022-03-25 17:23:53 +0300

    DIO-1095 "Statuspage button"
    (cherry picked from commit b5d58ca4ae86862d6230da19066119e836a5db29)

--- src/apps/dashboard/components/DashboardMenu/DashboardMenu.tsx
+++ src/apps/dashboard/components/DashboardMenu/DashboardMenu.tsx
@@ -4,13 +4,8 @@ import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { signOut } from 'apps/auth/state/auth.actions';
-import { TeamInviteModal } from 'apps/collaborators/components/TeamInviteModal/TeamInviteModal';
-import { collaboratorAdd } from 'apps/collaborators/state/collaborator.actions';
-import { selectCollaboratorState } from 'apps/collaborators/state/collaborator.selectors';
-import { IntlButton } from 'apps/intl';
+import { IntlButton, useFormatMessage } from 'apps/intl';
 import { TopMenuItem } from 'apps/layout';
-import { useOverlay } from 'apps/overlay';
-import { notification } from 'apps/ui';
 import { ReactComponent as MatiLogoWithText } from 'assets/metamap-logo-white.svg';
 import { ReactComponent as MatiLogo } from 'assets/metamap-logo-review.svg';
 
@@ -18,13 +13,14 @@ import classnames from 'classnames';
 import { QATags } from 'models/QA.model';
 import { Routes } from 'models/Router.model';
 import React, { useCallback, useEffect, useState } from 'react';
-import { FiChevronsLeft, FiChevronsRight, FiLogOut, FiPlusCircle, FiSettings } from 'react-icons/fi';
-import { useIntl } from 'react-intl';
+import { FiChevronsLeft, FiChevronsRight, FiLogOut, FiSettings } from 'react-icons/fi';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink, useHistory } from 'react-router-dom';
 import { selectIsOwnerModel, selectMerchantBusinessName } from 'state/merchant/merchant.selectors';
 import { useRole } from 'apps/collaborators';
 import { WithAuditor } from 'models/Collaborator.model';
+import { LiveStatusButton } from 'apps/liveStatus';
+import { Loadable } from 'models/Loadable.model';
 import { setIsDesktopMenuOpen } from '../../state/dashboard.actions';
 import { selectIsDesktopMenuOpen } from '../../state/dashboard.selectors';
 import { useLogout } from '../LogoutModal/LogoutModal';
@@ -33,57 +29,24 @@ import { SecondaryMenu } from '../SecondaryMenu/SecondaryMenu';
 import { useStyles } from './DashboardMenu.styles';
 
 export function DashboardMenu() {
-  const ownerModel = useSelector(selectIsOwnerModel);
+  const ownerModel = useSelector<any, Loadable<boolean>>(selectIsOwnerModel);
   const isOwner = ownerModel.isLoaded && ownerModel.value === true;
   const classes = useStyles();
-  const state = useSelector(selectCollaboratorState);
-  const intl = useIntl();
   const history = useHistory();
   const dispatch = useDispatch();
   const isDesktop = useMediaQuery('(min-width:768px)', { noSsr: true });
-  const isDesktopMenuOpen = useSelector(selectIsDesktopMenuOpen);
-  const [isOpen, setIsOpen] = useState(isDesktop && isDesktopMenuOpen);
-  const name = useSelector(selectMerchantBusinessName);
-  const [createOverlay, closeOverlay] = useOverlay();
+  const isDesktopMenuOpen = useSelector<any, boolean>(selectIsDesktopMenuOpen);
+  const [isOpen, setIsOpen] = useState<boolean>(isDesktop && isDesktopMenuOpen);
+  const name = useSelector<string>(selectMerchantBusinessName);
   const logout = useLogout();
   const role = useRole();
+  const formatMessage = useFormatMessage();
 
   const handleLogout = useCallback(async () => {
-    await logout(intl.formatMessage({ id: 'confirm_string' }));
+    await logout();
     dispatch(signOut());
     history.push(Routes.root);
-  }, [dispatch, history, intl, logout]);
-
-  const handleInviteSubmit = useCallback(async (data) => {
-    closeOverlay();
-    try {
-      await dispatch(collaboratorAdd({
-        role: parseInt(data.role, 10),
-        user: {
-          email: data.email,
-          firstName: data.firstName,
-          lastName: data.lastName,
-        },
-      }));
-      notification.info(intl.formatMessage({ id: 'teamTable.inviteSuccess.description' }));
-    } catch (error) {
-      notification.error(intl.formatMessage({
-        id: `Settings.teamSettings.submit.${error.response?.data?.name}`,
-        defaultMessage: intl.formatMessage({ id: 'Error.common' }),
-      }));
-      console.error(error);
-    }
-  }, [dispatch, closeOverlay, intl]);
-
-  const openInviteModal = useCallback(() => {
-    if (!isDesktop) {
-      setIsOpen(false);
-    }
-    createOverlay(<TeamInviteModal
-      onSubmit={handleInviteSubmit}
-      isPosting={state.isPosting}
-    />);
-  }, [handleInviteSubmit, state, isDesktop, setIsOpen, createOverlay]);
+  }, [dispatch, history, logout]);
 
   const toggleDrawerOpen = useCallback(() => {
     setIsOpen((prev) => !prev);
@@ -140,7 +103,7 @@ export function DashboardMenu() {
               startIcon={isOpen ? <FiChevronsLeft /> : <FiChevronsRight />}
               data-qa={QATags.Menu.RollUpButton}
             >
-              {intl.formatMessage({ id: 'dashboard.menu.rollUp' })}
+              {formatMessage('dashboard.menu.rollUp')}
             </Button>
           </Box>
           <Box p={2}>
@@ -154,6 +117,12 @@ export function DashboardMenu() {
           </Box>
           <Box>
             <SecondaryMenu isOwner={isOwner} isOpen={isOpen} />
+          </Box>
+          <Box p={2}>
+            <Divider className={classes.menuDivider} />
+          </Box>
+          <Box>
+            <LiveStatusButton />
           </Box>
         </Grid>
         <Grid item className={classes.contentBottom}>
@@ -170,27 +139,11 @@ export function DashboardMenu() {
                 </Box>
               )}
           </Box>
-          {isOwner && (
-            <Box pr={2} pl={1.5}>
-              <Button
-                className={classnames(classes.inviteButton, {
-                  [classes.inviteButtonSm]: !isOpen,
-                })}
-                variant="contained"
-                color="primary"
-                onClick={openInviteModal}
-                startIcon={<FiPlusCircle />}
-                data-qa={QATags.Menu.InviteTeammate}
-              >
-                {isOpen && intl.formatMessage({ id: 'settings.teamSettings.inviteTeammate' })}
-              </Button>
-            </Box>
-          )}
           <Box pt={1}>
             <TopMenuItem
               id="account"
               to={Routes.settings.root}
-              label={intl.formatMessage({ id: 'dashboard.menu.settings' })}
+              label={formatMessage('dashboard.menu.settings')}
               icon={<FiSettings />}
               color="common.black7"
               qa={QATags.Menu.Account}
@@ -209,7 +162,7 @@ export function DashboardMenu() {
               startIcon={<FiLogOut />}
               data-qa={QATags.Menu.Logout}
             >
-              {intl.formatMessage({ id: 'apps.settings.signout' })}
+              {formatMessage('apps.settings.signout')}
             </Button>
           </Box>
         </Grid>
