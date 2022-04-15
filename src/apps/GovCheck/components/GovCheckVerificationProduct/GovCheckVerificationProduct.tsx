d023ba84c7d87bc6f473396b1b86f05a11c5f1fc
anatoliy.turkin <anatoliy.turkin@mati.io>
2022-04-15 11:42:24 +0300

    fix govchecks verification container

--- src/apps/GovCheck/components/GovCheckVerificationProduct/GovCheckVerificationProduct.tsx
+++ src/apps/GovCheck/components/GovCheckVerificationProduct/GovCheckVerificationProduct.tsx
@@ -2,19 +2,16 @@ import Grid from '@material-ui/core/Grid';
 import { CheckStepDetails } from 'apps/checks';
 import { CheckBarExpandable, ChecksByDocument } from 'apps/ui';
 import React from 'react';
-import { useStyles } from './GovCheckVerificationProduct.styles';
 import { GovCheckText } from '../GovCheckText/GovCheckText';
 import { GovCheckVerificationData } from '../../models/GovCheck.model';
 
 export function GovCheckVerificationProduct({ data }: {
   data: GovCheckVerificationData;
 }) {
-  const classes = useStyles();
-
   return (
     <Grid container spacing={2}>
       {data.document?.map(({ govChecksSteps, type, country, photos }, index) => (
-        <Grid item xs={12} xl={4} className={classes.wrapper}>
+        <Grid item xs={8}>
           <ChecksByDocument photos={photos} country={country} docType={type} key={type || index}>
             {govChecksSteps?.map((step) => (
               <CheckBarExpandable step={step} key={step.title} title={step.title}>
@@ -27,7 +24,7 @@ export function GovCheckVerificationProduct({ data }: {
         </Grid>
       ))}
       {data.govCheckWithoutDocument?.map((step) => (
-        <Grid item xs={12} xl={4} className={classes.wrapper}>
+        <Grid item xs={8}>
           <CheckBarExpandable step={step} key={step.id}>
             <CheckStepDetails step={step}>
               <GovCheckText step={step} />
