84b38b826e30a32b7b7cfc4da9ff6a669fd8187e
VRIZAOT <otabekbutcher@gmail.com>
2022-04-21 12:34:08 +0500

    feat: redesign mx background check lcw-220

---
+++ src/apps/BackgroundCheck/models/BackgroundCheck.model.ts
@@ -0,0 +1,10 @@
+export type BackgroundCheckExpandableCellTypes = {
+  juzgado: string;
+  actor: string;
+  demandado: string;
+  expediente: string;
+  tipo: string;
+  fuero: string;
+  fecha: string;
+  acuerdos: any;
+}
